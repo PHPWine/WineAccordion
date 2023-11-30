f047f85c1a4e3afdeb52308682b3a62f0516571c
nielsoffice <nieldigitalsolution@gmail.com>
2023-11-30 17:54:17 +0800

    Update WineAccordion.php

--- src/WineAccordion.php
+++ src/WineAccordion.php
@@ -5,7 +5,7 @@ namespace PHPWineAccordion;
 ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
- 
+
 /**
 * @copyright (c) 2023 PHPWine Component Accordion Cooked by nielsoffice
 *
